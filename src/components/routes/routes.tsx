ff18132aaf0593a889c4692890b012e31fe36d31
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2021-04-15 14:20:49 +0200

    [2.0][fix] - adding index.css

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Login from '../../views/Login/Login'
+import Login from '../../views/Login/login'
+import Providers from '../../views/Console-admin/Providers/providers'
 
 const Routes = () => {
     return (
         <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Providers} />
         </Switch>
     </BrowserRouter>
     )
