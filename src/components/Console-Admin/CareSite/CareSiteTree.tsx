e298ee3f5dcfaa9149572a04e89abdd090ba7ed4
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2021-04-28 16:18:49 +0200

    [2.0][feat] comment CareSite.tsx and CareSiteTree.tsx

--- src/components/Console-Admin/CareSite/CareSiteTree.tsx
+++ src/components/Console-Admin/CareSite/CareSiteTree.tsx
@@ -1,97 +1,100 @@
-import React, { useEffect, useState } from "react"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import { useDispatch, useSelector } from "react-redux"
-import { makeStyles } from "@material-ui/core/styles"
-import MaterialTable from "material-table"
-import Grid from "@material-ui/core/Grid"
-import * as _ from "lodash"
+const test = () => {}
 
-import { submitGetCareSites } from "services/Console-Admin/careSiteService"
+export default test
+// import React, { useEffect, useState } from "react"
+// import CircularProgress from "@material-ui/core/CircularProgress"
+// import { useDispatch, useSelector } from "react-redux"
+// import { makeStyles } from "@material-ui/core/styles"
+// import MaterialTable from "material-table"
+// import Grid from "@material-ui/core/Grid"
+// import * as _ from "lodash"
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    height: "calc(100% - 84px)",
-    display: "flex",
-    justifyContent: "space-between",
-    flexDirection: "column",
-    overflow: "auto",
-  },
-  mainRow: {},
-  secondRow: {},
-}))
+// import { submitGetCareSites } from "services/Console-Admin/careSiteService"
 
-const careSiteToRow = (cs: CareSite) => ({
-  id: cs.id.toString(),
-  careSiteId: cs.id,
-  name: cs.name,
-  shortName: cs.shortName,
-  parentId: cs.parentId,
-})
+// const useStyles = makeStyles((theme) => ({
+//   container: {
+//     height: "calc(100% - 84px)",
+//     display: "flex",
+//     justifyContent: "space-between",
+//     flexDirection: "column",
+//     overflow: "auto",
+//   },
+//   mainRow: {},
+//   secondRow: {},
+// }))
 
-const CareSitesTree: React.FC = () => {
-  const classes = useStyles()
-  const dispatch = useDispatch()
+// const careSiteToRow = (cs: CareSite) => ({
+//   id: cs.id.toString(),
+//   careSiteId: cs.id,
+//   name: cs.name,
+//   shortName: cs.shortName,
+//   parentId: cs.parentId,
+// })
 
-  const [loading, setLoading] = useState(true)
+// const CareSitesTree: React.FC = () => {
+//   const classes = useStyles()
+//   const dispatch = useDispatch()
 
-  useEffect(() => {
-    const _init = async () => {
-      setLoading(true)
-      await fetchCareSitesTree()
-      setLoading(false)
-    }
+//   const [loading, setLoading] = useState(true)
 
-    _init()
-  }, []) // eslint-disable-line
+//   useEffect(() => {
+//     const _init = async () => {
+//       setLoading(true)
+//       await fetchCareSitesTree()
+//       setLoading(false)
+//     }
 
-  const columns = [
-    {
-      title: "Nom",
-      field: "name",
-      emptyValue: "-",
-    },
-    {
-      title: "Sigle",
-      field: "shortName",
-      emptyValue: "-",
-      filtering: false,
-    },
-  ]
+//     _init()
+//   }, []) // eslint-disable-line
 
-  return (
-    <div className={classes.container}>
-      {loading ? (
-        <Grid container justify="center">
-          <CircularProgress size={50} />
-        </Grid>
-      ) : (
-        <MaterialTable
-          columns={columns}
-          data={_.values(careSites).map(careSiteToRow)}
-          parentChildData={({ parentId }, rows) =>
-            rows.find(({ careSiteId }) => careSiteId === parentId)
-          }
-          options={{
-            paging: false,
-            filtering: false,
-            search: false,
-            showTitle: false,
-            toolbar: false,
-            headerStyle: {
-              fontSize: 11,
-              color: "#0063AF",
-              padding: "0 20px",
-              backgroundColor: "#D1E2F4",
-              height: 42,
-              fontWeight: "bold",
-              textTransform: "uppercase",
-            },
-          }}
-          onRowClick={(e, row) => window.open(`/care-sites/${row?.id}`)}
-        />
-      )}
-    </div>
-  )
-}
+//   const columns = [
+//     {
+//       title: "Nom",
+//       field: "name",
+//       emptyValue: "-",
+//     },
+//     {
+//       title: "Sigle",
+//       field: "shortName",
+//       emptyValue: "-",
+//       filtering: false,
+//     },
+//   ]
 
-export default CareSitesTree
+//   return (
+//     <div className={classes.container}>
+//       {loading ? (
+//         <Grid container justify="center">
+//           <CircularProgress size={50} />
+//         </Grid>
+//       ) : (
+//         <MaterialTable
+//           columns={columns}
+//           data={_.values(careSites).map(careSiteToRow)}
+//           parentChildData={({ parentId }, rows) =>
+//             rows.find(({ careSiteId }) => careSiteId === parentId)
+//           }
+//           options={{
+//             paging: false,
+//             filtering: false,
+//             search: false,
+//             showTitle: false,
+//             toolbar: false,
+//             headerStyle: {
+//               fontSize: 11,
+//               color: "#0063AF",
+//               padding: "0 20px",
+//               backgroundColor: "#D1E2F4",
+//               height: 42,
+//               fontWeight: "bold",
+//               textTransform: "uppercase",
+//             },
+//           }}
+//           onRowClick={(e, row) => window.open(`/care-sites/${row?.id}`)}
+//         />
+//       )}
+//     </div>
+//   )
+// }
+
+// export default CareSitesTree
--- src/views/Console-Admin/CareSite/CareSite.tsx
+++ src/views/Console-Admin/CareSite/CareSite.tsx
@@ -1,26 +1,29 @@
-import React from "react"
-import { Grid, Paper, Typography } from "@material-ui/core"
+const test2 = () => {}
 
-import CareSitesTree from "components/Console-Admin/CareSite/CareSiteTree"
-import useStyles from "./styles"
+export default test2
+// import React from "react"
+// import { Grid, Paper, Typography } from "@material-ui/core"
 
-const CareSites = () => {
-  const classes = useStyles()
+// import CareSitesTree from "components/Console-Admin/CareSite/CareSiteTree"
+// import useStyles from "./styles"
 
-  return (
-    <Grid container direction="column">
-      <Grid container justify="center" alignItems="center">
-        <Grid container item xs={12} sm={9} direction="column">
-          <Typography variant="h1" color="primary" className={classes.title}>
-            Liste des périmètres
-          </Typography>
-          <Paper>
-            <CareSitesTree />
-          </Paper>
-        </Grid>
-      </Grid>
-    </Grid>
-  )
-}
+// const CareSites = () => {
+//   const classes = useStyles()
 
-export default CareSites
+//   return (
+//     <Grid container direction="column">
+//       <Grid container justify="center" alignItems="center">
+//         <Grid container item xs={12} sm={9} direction="column">
+//           <Typography variant="h1" color="primary" className={classes.title}>
+//             Liste des périmètres
+//           </Typography>
+//           <Paper>
+//             <CareSitesTree />
+//           </Paper>
+//         </Grid>
+//       </Grid>
+//     </Grid>
+//   )
+// }
+
+// export default CareSites
