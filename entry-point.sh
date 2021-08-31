9cd2ef248468cd49c1dcdf28f7b7372d8e6e71c3
bplaye <benoit.playe-ext@aphp.fr>
2021-08-31 18:07:41 +0200

    [2.0][fix] - fix anonymize url

--- entry-point.sh
+++ entry-point.sh
@@ -2,9 +2,11 @@
 
 # @todo: remove the following line when cache is available
 npm install && npm audit fix && CI=false npm run build
-envsubst < /etc/nginx/sites-enabled/nginx.conf > nginx_conf && mv nginx_conf /etc/nginx/sites-enabled/nginx.conf
+sed -i s/{{JWT_SERVER}}/$JWT_SERVER/g /etc/nginx/sites-enabled/nginx.conf
+sed -i s/{{PORTAIL_SERVER}}/$PORTAIL_SERVER/g /etc/nginx/sites-enabled/nginx.conf
 
-#envsubst < /etc/nginx/conf.d/nginx.conf > nginx_conf && mv nginx_conf /etc/nginx/conf.d/nginx.conf
+#sed -i s/{{JWT_SERVER}}/$JWT_SERVER/g /etc/nginx/conf.d/nginx.conf
+#sed -i s/{{PORTAIL_SERVER}}/$PORTAIL_SERVER/g /etc/nginx/conf.d/nginx.conf
 
 service nginx restart
 sleep infinity
