d8f537e4e1e82ce29b217295862fdffea897f51e
bplaye <benoit.playe-ext@aphp.fr>
2021-08-31 15:53:02 +0200

    [2.0][fix] - fix build: remove cache opt

--- entry-point.sh
+++ entry-point.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # remove the following line when cache is available
-npm install && npm audit fix && npm run build
+npm install && npm audit fix && CI=false npm run build
 
 service nginx restart
 sleep infinity
