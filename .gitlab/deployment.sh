a51625c0b93b15e986fd336b6bea8be5f5810b85
bplaye <benoit.playe-ext@aphp.fr>
2021-08-31 15:41:37 +0200

    [2.0][improvment] - clean k8s deploy

--- .gitlab/deployment.sh
+++ .gitlab/deployment.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # replace placeholders by env var
-
-sed -i "s/{{ENVIR}}/$ENVIR/g" nginx.conf
+ENVIRONMENT=${CI_COMMIT_BRANCH/_/-}
+sed -i "s/{{ENVIR}}/$CI_COMMIT_BRANCH/g" nginx.conf
 sed -i "s/{{ENVIRONMENT}}/$ENVIRONMENT/g" nginx.conf
-sed -i "s/{{ENVIR}}/$ENVIR/g" Dockerfile
+sed -i "s/{{ENVIR}}/$CI_COMMIT_BRANCH/g" Dockerfile
 
 # exit if all variables are not replaced
 if find ./ -type f \( -name "nginx.conf" -o -name "Dockerfile" \) -print0 | xargs -0  grep -Fq {{
@@ -17,4 +17,4 @@ else
 fi
 
 echo "{\"auths\":{\"$HARBOR_REGISTRY\":{\"username\":\"$BOT_NAME\",\"password\":\"$BOT_TOKEN\"}}}" > /kaniko/.docker/config.json
-/kaniko/executor --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile --destination $HARBOR_REPOSITORY_URL/portail:$ENVIR
+/kaniko/executor --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile_tmp --destination $HARBOR_REPOSITORY_URL/portail:$CI_COMMIT_BRANCH
--- entry-point.sh
+++ entry-point.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
+# remove the following line when cache is available
+npm install && npm audit fix && npm run build
+
 service nginx restart
 sleep infinity
