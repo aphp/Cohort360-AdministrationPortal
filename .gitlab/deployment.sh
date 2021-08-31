be62e199f8d613b7f25426f4d736dda39b9c0783
bplaye <benoit.playe-ext@aphp.fr>
2021-08-31 17:40:46 +0200

    [2.0][fix] - anonymize url

--- .gitlab/deployment.sh
+++ .gitlab/deployment.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/bash
 
-# replace placeholders by env var
 ENVIRONMENT=${CI_COMMIT_BRANCH/_/-}
-sed -i "s/{{ENVIR}}/$CI_COMMIT_BRANCH/g" nginx.conf
-sed -i "s/{{ENVIRONMENT}}/$ENVIRONMENT/g" nginx.conf
-sed -i "s/{{ENVIR}}/$CI_COMMIT_BRANCH/g" Dockerfile
 
-# exit if all variables are not replaced
-if find ./ -type f \( -name "nginx.conf" -o -name "Dockerfile" \) -print0 | xargs -0  grep -Fq {{
-then
-    echo "some variables are not replaced"
-    find ./ -type f \( -name "nginx.conf" -o -name "Dockerfile" \) -print0 | xargs -0  grep -FH {{
-    exit 1
-else
-    echo "All variables have been well replaced"
-fi
-
-# @todo: rm after cache is enabled
+# @todo: rm after cache is enabled + CHANGE Dockerfile_tmp FOR Dockerfile
 sed -i "s/{{ENVIR}}/$CI_COMMIT_BRANCH/g" Dockerfile_tmp
-
 echo "{\"auths\":{\"$HARBOR_REGISTRY\":{\"username\":\"$BOT_NAME\",\"password\":\"$BOT_TOKEN\"}}}" > /kaniko/.docker/config.json
 /kaniko/executor --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile_tmp --destination $HARBOR_REPOSITORY_URL/portail:$CI_COMMIT_BRANCH
+
+#sed -i "s/{{ENVIR}}/$CI_COMMIT_BRANCH/g" Dockerfile
+#echo "{\"auths\":{\"$HARBOR_REGISTRY\":{\"username\":\"$BOT_NAME\",\"password\":\"$BOT_TOKEN\"}}}" > /kaniko/.docker/config.json
+#/kaniko/executor --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile --destination $HARBOR_REPOSITORY_URL/portail:$CI_COMMIT_BRANCH
--- entry-point.sh
+++ entry-point.sh
@@ -2,6 +2,9 @@
 
 # @todo: remove the following line when cache is available
 npm install && npm audit fix && CI=false npm run build
+envsubst < /etc/nginx/sites-enabled/nginx.conf > nginx_conf && mv nginx_conf /etc/nginx/sites-enabled/nginx.conf
+
+#envsubst < /etc/nginx/conf.d/nginx.conf > nginx_conf && mv nginx_conf /etc/nginx/conf.d/nginx.conf
 
 service nginx restart
 sleep infinity
