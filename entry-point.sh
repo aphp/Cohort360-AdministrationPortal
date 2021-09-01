142c614f1e596261eb7fbecc70b9343bf4ad7cad
bplaye <benoit.playe-ext@aphp.fr>
2021-09-01 10:03:40 +0200

    [2.0][fix] - fix anonymize url

--- entry-point.sh
+++ entry-point.sh
@@ -2,11 +2,16 @@
 
 # @todo: remove the following line when cache is available
 npm install && npm audit fix && CI=false npm run build
-sed -i s/{{JWT_SERVER}}/$JWT_SERVER/g /etc/nginx/sites-enabled/nginx.conf
-sed -i s/{{PORTAIL_SERVER}}/$PORTAIL_SERVER/g /etc/nginx/sites-enabled/nginx.conf
 
-#sed -i s/{{JWT_SERVER}}/$JWT_SERVER/g /etc/nginx/conf.d/nginx.conf
-#sed -i s/{{PORTAIL_SERVER}}/$PORTAIL_SERVER/g /etc/nginx/conf.d/nginx.conf
+declare -a UrlArray=("JWT_URL" "PORTAIL_URL")
+
+for NAME in ${UrlArray[@]}; do
+  ENV_VAR="${!NAME}"
+  ENV_VAR=$(echo $ENV_VAR | sed "s/[^a-zA-Z0-9]/\\\\&/g");
+  sed -i s/"{{"$NAME"}}"/$ENV_VAR/g /etc/nginx/sites-enabled/nginx.conf;
+#  sed -i s/"{{"$NAME"}}"/$ENV_VAR/g /etc/nginx/conf.d/nginx.conf;
+done
 
 service nginx restart
+
 sleep infinity
