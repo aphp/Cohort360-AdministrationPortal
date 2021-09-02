a8a38bb07b7f1036baa3f93fb2d9fa71147fffd6
manelleg <manelle.gueriouz@aphp.fr>
2021-09-02 10:16:40 +0200

    [2.0][bug-fix] - fixed interrupt to close

--- src/components/Console-Admin/Rights/RightsTable/RightsTable.tsx
+++ src/components/Console-Admin/Rights/RightsTable/RightsTable.tsx
@@ -106,7 +106,7 @@ const RightsTable: React.FC<RightsTableProps> = ({
       setDeleteAccess(null)
     } catch (error) {
       console.error(
-        "Erreur lors de la suppression ou l'interruption de l'accès",
+        "Erreur lors de la suppression ou la clôture de l'accès",
         error
       )
       setDeleteAccessFail(true)
@@ -242,7 +242,7 @@ const RightsTable: React.FC<RightsTableProps> = ({
                           {access.actual_start_datetime &&
                             moment(access.actual_start_datetime).isBefore() &&
                             access.is_valid && (
-                              <Tooltip title="Interrompre l'accès">
+                              <Tooltip title="Clôturer l'accès">
                                 <IconButton
                                   onClick={() => {
                                     setDeleteAccess(access)
@@ -307,7 +307,7 @@ const RightsTable: React.FC<RightsTableProps> = ({
         <DialogContent>
           <Typography>
             Êtes-vous sûr(e) de vouloir{" "}
-            {terminateAccess ? "interrompre" : "supprimer"} cet accès sur le
+            {terminateAccess ? "clôturer" : "supprimer"} cet accès sur le
             périmètre {deleteAccess?.care_site.care_site_name} ?
           </Typography>
         </DialogContent>
@@ -330,9 +330,7 @@ const RightsTable: React.FC<RightsTableProps> = ({
         >
           {editAccessSuccess && "Les dates d'accès ont bien été éditées."}
           {deleteAccessSuccess &&
-            `L'accès a bien été ${
-              terminateAccess ? "interrompu" : "supprimé"
-            }.`}
+            `L'accès a bien été ${terminateAccess ? "clôturé" : "supprimé"}.`}
         </Alert>
       )}
       {(editAccessFail || deleteAccessFail) && (
@@ -346,8 +344,8 @@ const RightsTable: React.FC<RightsTableProps> = ({
         >
           {editAccessFail && "Erreur lors de l'édition de l'accès."}
           {deleteAccessFail &&
-            `Erreur lors de ${
-              terminateAccess ? "l'interruption" : "la suppression"
+            `Erreur lors de la ${
+              terminateAccess ? "clôture" : "suppression"
             } de l'accès.`}
         </Alert>
       )}
