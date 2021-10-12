2c6081c686c56bb8e3df950a9b675bfee964daa3
manelleg <manelle.gueriouz@aphp.fr>
2021-10-12 17:24:32 +0200

    [2.0][fix] - changed alerts with snackbars that disappear after 3sc

--- src/components/Console-Admin/Rights/styles.ts
+++ src/components/Console-Admin/Rights/styles.ts
@@ -1,24 +1,18 @@
-import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(() =>
   createStyles({
     searchButton: {
-      backgroundColor: '#5BC5F2',
-      color: '#FFF',
+      backgroundColor: "#5BC5F2",
+      color: "#FFF",
       borderRadius: 25,
       height: 50,
-      margin: '4px 0'
+      margin: "4px 0",
     },
     title: {
-      margin: "12px 0"
+      margin: "12px 0",
     },
-    alert: {
-      position: "fixed",
-      bottom: 0,
-      right: 0,
-      zIndex: 100
-    }
   })
-);
+)
 
-export default useStyles;
+export default useStyles
