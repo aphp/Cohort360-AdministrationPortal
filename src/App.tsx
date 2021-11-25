f3b791cc7846be9de52692fa8b2618ef4b2250b7
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2021-11-25 14:25:43 +0100

    [2.2][fix] - remove app.css

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Provider } from "react-redux"
 
 import AppNavigation from "./components/routes/AppNavigation/AppNavigation"
 import { store } from "./state/store"
-import "./App.css"
 import MomentUtils from "@date-io/moment"
 import { MuiPickersUtilsProvider } from "@material-ui/pickers"
 
@@ -15,14 +14,12 @@ moment.locale("fr")
 
 const App = () => {
   return (
-    <div className="App">
       <MuiPickersUtilsProvider utils={MomentUtils}>
         <Provider store={store}>
           <CssBaseline />
           <AppNavigation />
         </Provider>
       </MuiPickersUtilsProvider>
-    </div>
   )
 }
 
