1082f47720b3cce7ca5eff6034ee49eb7609a91d
Alizé Etchegorry <alize.etchegorry@aphp.fr>
2023-05-16 16:27:54 +0000

    refactor: switch CRA viteJS - Ref gestion-de-projet#2061

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 sed -i "s@{JWT_URL}@$JWT_URL@g" /etc/nginx/conf.d/nginx.conf
 sed -i "s@{BACK_URL}@$BACK_URL@g" /etc/nginx/conf.d/nginx.conf
 
-sed -i "s@{BACK_API_URL}@$BACK_API_URL@g" /app/build/static/js/*.js
+sed -i "s@{VITE_BACK_API_URL}@$VITE_BACK_API_URL@g" /app/build/assets/*.js
 
 service nginx restart
 
